0821af9670eaefc50d93d6d2948ae2960b7e0ac9
Axel Scheele <axel@Axels-MacBook-Pro.local>
2017-03-13 20:23:26 +0100

    setup for slack

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 var SlackBots = require('slackbots')
-var TelegramBot = require('telegraf')
+
 var request = require('request')
 
 var botSlack = new SlackBots({
@@ -7,15 +7,3 @@ var botSlack = new SlackBots({
     name: 'General Stavoros'
 });
 
-var botTelegram = new TelegramBot(process.env.TELEGRAMTOKEN);
-
-botTelegram.command('start', (ctx) => {
-  console.log('start', ctx.from)
-  ctx.reply('Welcome!')
-})
-
-botTelegram.hears('hi', (ctx) => ctx.reply('WHAHAHAHAHAHAHA'))
-
-botTelegram.on('sticker', (ctx) => ctx.reply('KEKEKEKEKEKEKEKE'))
-
-botTelegram.startPolling()
